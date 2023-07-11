136e58d79547b6d9127cf343e314b7adf622656f
lu78abranches <luisabranches.surf@hotmail.com>
2023-07-11 00:53:42 -0300

    Initial Commit

---
+++ src/main/java/com/devsuperior/dsline/DslineApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dsline/DslineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
