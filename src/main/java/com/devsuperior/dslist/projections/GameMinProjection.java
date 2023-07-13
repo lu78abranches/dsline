d19c1c18d21e8b2540f58ed2227fd15e44f5d787
lu78abranches <luisabranches.surf@hotmail.com>
2023-07-13 00:07:15 -0300

    homologation ok

--- src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -4,7 +4,7 @@ public interface GameMinProjection { //interface para consulta SQL customizada
 
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
